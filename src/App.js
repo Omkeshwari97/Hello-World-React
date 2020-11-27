c59483570a6553f51cb1ff41bb72e1c4550ad75d
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-11-27 10:39:25 +0530

    React functionalities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*class App extends React.Component{
+  render() {
+    return (
+      <div>
+        <h1>Hello from Bridgelabz</h1>
+      </div>
+    );
+  }
+}*/
+
+
+class App extends React.Component{
+  url="http://bridgelabz.com/";
+  constructor(){
+    super()
+    this.state = {
+      //title : "Hello from Bridgelabz"
+      userName:''
+    }
+  }
+
+  onClick=($event)=>{
+    console.log("save button is clicked", $event);
+    window.open(this.url,"_blank");
+  }
+
+  onNameChange=(event)=>{
+    console.log("Value is ", event.target.value);
+    this.setState({userName: event.target.value});
+  }
+
+  render(){
+    return(
+      <div>
+      <div>
+        <h1>{this.state.userName} from Bridgelabz</h1>
+        <img src={logo} onClick={this.onClick} alt="Bridgelabz gives employment"/>
+      </div>
+      <div className="text-box"> 
+        <input onChange={this.onNameChange}/>
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
